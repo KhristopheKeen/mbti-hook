724f6937802565cc0b3bab8cf284719605146a81
PortraitstoneLabs <khristophe.keen@gmail.com>
2022-02-04 10:02:01 -0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Random from "./components/Random_V1";
+import Tag from "./components/Tag_V1";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+    <div>
+        <h1>Random GIF Getter</h1>
+        <div className="main-container">
+            <Random/>
+            <Tag/>
+        </div>
     </div>
-  );
-}
+);
 
 export default App;
